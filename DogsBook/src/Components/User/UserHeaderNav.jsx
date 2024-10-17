76e355c34387242c5502e8115a4e2cdf1b81cd0b
EmanuelMarquessf <emanuel.marquessf@gmail.com>
2024-10-17 19:41:38 -0300

    Mobile menu

--- DogsBook/src/Components/User/UserHeaderNav.jsx
+++ DogsBook/src/Components/User/UserHeaderNav.jsx
@@ -1,41 +1,65 @@
-import React, { useContext, useState } from "react";
-import { NavLink } from "react-router-dom";
-import { UserContext } from "../../UserContext";
-import MinhasFotos from "../../Assets/feed.svg?react";
-import Estatisticas from "../../Assets/estatisticas.svg?react";
-import AdicionarFoto from "../../Assets/adicionar.svg?react";
-import Sair from "../../Assets/sair.svg?react";
-import style from "./UserHeaderNav.module.css";
+import React from 'react';
+import { NavLink, useLocation, useNavigate } from 'react-router-dom';
+import { UserContext } from '../../UserContext';
+import MinhasFotos from '../../Assets/feed.svg?react';
+import Estatisticas from '../../Assets/estatisticas.svg?react';
+import AdicionarFoto from '../../Assets/adicionar.svg?react';
+import Sair from '../../Assets/sair.svg?react';
+import styles from './UserHeaderNav.module.css';
+import useMedia from '../../Hooks/useMedia';
 
-function UserHeaderNav() {
-  const { userLogout } = useContext(UserContext);
-  const [mobile, setMobile] = useState(null);
+const UserHeaderNav = () => {
+  const { userLogout } = React.useContext(UserContext);
+  const mobile = useMedia('(max-width: 40rem)');
+  const [mobileMenu, setMobileMenu] = React.useState(false);
+  const navigate = useNavigate();
+
+  const { pathname } = useLocation();
+  React.useEffect(() => {
+    setMobileMenu(false);
+  }, [pathname]);
 
   function handleLogout() {
     userLogout();
-    navigate("/login");
+    navigate('/login');
   }
 
   return (
-    <nav className={style.nav}>
-      <NavLink to="/conta" end>
-        <MinhasFotos />
-        {mobile && "Minhas fotos"}
-      </NavLink>
-      <NavLink to="/conta/estatisticas">
-        <Estatisticas />
-        {mobile && "Estatisticas"}
-      </NavLink>
-      <NavLink to="/conta/postar">
-        <AdicionarFoto />
-        {mobile && "Adicionar Foto"}
-      </NavLink>
-      <button onClick={handleLogout}>
-        <Sair />
-        {mobile && "Sair"}
-      </button>
-    </nav>
+    <>
+      {mobile && (
+        <button
+          aria-label="Menu"
+          className={`${styles.mobileButton} ${
+            mobileMenu && styles.mobileButtonActive
+          }`}
+          onClick={() => setMobileMenu(!mobileMenu)}
+        ></button>
+      )}
+
+      <nav
+        className={`${mobile ? styles.navMobile : styles.nav} ${
+          mobileMenu && styles.navMobileActive
+        }`}
+      >
+        <NavLink to="/conta" end>
+          <MinhasFotos />
+          {mobile && 'Minhas Fotos'}
+        </NavLink>
+        <NavLink to="/conta/estatisticas">
+          <Estatisticas />
+          {mobile && 'Estatísticas'}
+        </NavLink>
+        <NavLink to="/conta/postar">
+          <AdicionarFoto />
+          {mobile && 'Adicionar Foto'}
+        </NavLink>
+        <button onClick={handleLogout}>
+          <Sair />
+          {mobile && 'Sair'}
+        </button>
+      </nav>
+    </>
   );
-}
+};
 
 export default UserHeaderNav;
